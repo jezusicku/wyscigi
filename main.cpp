86304b946bf6bf1434fb0656e180ec800f8819cd
Paweł Duraj <dgpawelduraj@gmail.com>
2021-05-22 00:16:08 +0200

    Partial fix of fps drops

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ int main() {
     Menu menu(&settings);
     Engine engine(&settings);
 
+    sf::Clock clock;
+    float lastTime = 0.0f;
+    float currTime = 0.0f;
+
     sf::Event event{};
     int *scene = settings.getScenePointer();
     bool *focus = settings.getFocusPointer();
@@ -20,6 +24,9 @@ int main() {
     *scene = 0; // 1 - load game, 0 - load menu
 
     while (window.isOpen() && *scene >= 0) {
+
+        currTime = clock.getElapsedTime().asSeconds();
+
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -39,7 +46,7 @@ int main() {
         if (*scene == 0) {
             menu.display();
         } else if (*scene == 1) {
-            engine.update();
+            if (currTime - lastTime >= 1.0f / 60.0f) engine.update();
             engine.display();
         }
 
