91d6684fa83d7ea7377fbbb22679f412705bdc19
Paweł Duraj <dgpawelduraj@gmail.com>
2021-05-22 13:19:44 +0200

    Add pause menu

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -6,24 +6,32 @@ Engine::Engine(Settings *settings) {
     this->scene = settings->getScenePointer();
     this->speed = 20;
     this->counter = 0;
+    this->pause = false;
     this->player.init(settings);
-    this->player.setSpeedPlayer(speed);
+    this->player.setSpeedPlayer(float(speed));
     this->map.init(settings, &speed);
     this->hud.init(settings, &speed);
+    this->pauseMenu.init(settings, &pause);
 }
 
 void Engine::display() {
     map.display();
     player.display();
     hud.display();
+    if (pause) pauseMenu.display();
 }
 
 void Engine::update() {
-    map.update();
-    player.update();
-    if (++counter == 100 and speed < 60) {
-        counter = 0;
-        speed++;
+    if (!*settings->getFocusPointer()) pause = true;
+    if (!pause) {
+        map.update();
+        player.update();
+        if (++counter == 100 and speed < 60) {
+            counter = 0;
+            speed++;
+        }
+        hud.update();
+    } else {
+        pauseMenu.update();
     }
-    hud.update();
 }
